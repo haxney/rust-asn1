92c422daeb6d5bc17a215504f68f4770d9a416e0
Daniel Hackney <dan@haxney.org>
2016-04-12 23:46:02 -0700

    Create working parser for realnumber

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
-#[test]
-fn it_works() {
+//! Taken from http://www.itu.int/rec/T-REC-X.680-200811-I
+
+#![allow(dead_code)] // Until parser is complete. Too noisy otherwise
+
+#[macro_use]
+extern crate nom;
+
+extern crate regex;
+
+use std::str::FromStr;
+
+/// Parse an `f64`.
+///
+/// Defined in X.680 §12.9
+named!(realnumber<&str, f64>,
+   map_res!(
+       re_find!(r"^\d+(\.\d*)?([eE]-?(0|[1-9]\d*))?"),
+       FromStr::from_str
+   )
+);
+
+#[cfg(test)]
+mod tests {
+
+    use super::realnumber;
+    use nom::IResult;
+    use nom::IResult::Done;
+
+    /// Simple way of specifying an `IResult::Done` with no remaining input.
+    macro_rules! done_result (
+        ($res:expr) => (
+            IResult::Done("", $res)
+        )
+    );
+
+    #[test]
+    fn test_realnumber() {
+        assert_eq!(realnumber("1"), done_result!(1.0));
+        assert_eq!(realnumber("0"), done_result!(0.0));
+
+        // Can have a leading zero
+        assert_eq!(realnumber("00"), done_result!(0.0));
+        assert_eq!(realnumber("01"), done_result!(1.0));
+
+        // Can have a trailing period without a factional part
+        assert_eq!(realnumber("1."), done_result!(1.0));
+        assert_eq!(realnumber("1.0"), done_result!(1.0));
+        assert_eq!(realnumber("1.001"), done_result!(1.001));
+
+        // Exponents
+        assert_eq!(realnumber("1.1e3"), done_result!(1100.0));
+        assert_eq!(realnumber("1.1e-3"), done_result!(0.0011));
+        assert_eq!(realnumber("1.e3"), done_result!(1000.0));
+        assert_eq!(realnumber("1e3"), done_result!(1000.0));
+        assert_eq!(realnumber("1.1E3"), done_result!(1100.0));
+        assert_eq!(realnumber("1.1E-3"), done_result!(0.0011));
+
+        // Leading zero in exponent
+        assert_eq!(realnumber("1.1E-03"), Done("3", 1.1));
+        assert_eq!(realnumber("1.1E03"), Done("3", 1.1));
+    }
+
 }
